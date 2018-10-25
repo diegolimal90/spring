89da8fe91f29a6486a5a33c97c9794dd6061699c
diegolimal90 <diego.lima.lopes@hotmail.com.br>
2018-10-25 00:52:23 -0300

    endpoint/pedido/id disponibilizado

---
+++ src/main/java/com/diegolima/cursomc/services/PedidoService.java
@@ -0,0 +1,24 @@
+package com.diegolima.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.diegolima.cursomc.domain.Pedido;
+import com.diegolima.cursomc.repositories.PedidoRepository;
+import com.diegolima.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Pedido obj = repo.findOne(id);
+		if(obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id 
+					+ ", Tipo: " + Pedido.class.getName());
+		}
+		return obj;
+	}
+}
